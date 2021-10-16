b03dafcb8fa2f7c6596fd5e836ad984fbd4b9606
neatc0der <grosch.tobias@googlemail.com>
2021-10-16 21:16:13 +0200

    Bumps version to v2.1.3

--- mkdocs_markmap/__meta__.py
+++ mkdocs_markmap/__meta__.py
@@ -1,6 +1,6 @@
 PACKAGE_NAME = 'mkdocs_markmap'
 PROJECT_NAME = PACKAGE_NAME.replace('_', '-')
-PROJECT_VERSION = '2.1.2'
+PROJECT_VERSION = '2.1.3'
 
 OWNER = 'neatc0der'
 REPOSITORY_NAME = f'{OWNER}/{PROJECT_NAME}'
