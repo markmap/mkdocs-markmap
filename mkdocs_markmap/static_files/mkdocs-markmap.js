b103ceb5a2ad042ec991d247da96319c87171851
neatc0der <grosch.tobias@googlemail.com>
2021-11-04 22:04:22 +0100

    #26: Adjusts javascript to support page update by decryption event

--- mkdocs_markmap/static_files/mkdocs-markmap.js
+++ mkdocs_markmap/static_files/mkdocs-markmap.js
@@ -1,19 +1,33 @@
 (function initializeMarkmap() {
-    const markmap_transformer = new markmap.Transformer();
-    const markmaps = document.getElementsByClassName('mkdocs-markmap');
-    var el, content, svg, root, m;
-    for (var i = 0; i < markmaps.length; i++) {
-        el = markmaps[i];
-        content = el.getAttribute('data-markdown').replaceAll('&#10;', '\n');
-        svg = el.querySelector('svg');
-        root = markmap_transformer.transform(content).root;
-        m = markmap.Markmap.create(svg, null, root);
+    function update_markmaps() {
+        const markmap_transformer = new markmap.Transformer();
+        const markmaps = document.getElementsByClassName('mkdocs-markmap');
+        var el, content, svg, root, m;
+        for (var i = 0; i < markmaps.length; i++) {
+            el = markmaps[i];
+            content = el.getAttribute('data-markdown').replaceAll('&#10;', '\n');
+            svg = el.querySelector('svg');
+            root = markmap_transformer.transform(content).root;
+            m = markmap.Markmap.create(svg, null, root);
 
-        (function(obj, e, r) {
-            obj.rescale(1).then(function() {
-                e.parentElement.style.height = (e.getBBox().height + 10) + "px";
-                requestAnimationFrame(() => { obj.fit(); })
-            });
-        })(m, svg, root);
+            (function(obj, e, r) {
+                obj.rescale(1).then(function() {
+                    e.parentElement.style.height = (e.getBBox().height + 10) + "px";
+                    requestAnimationFrame(() => { obj.fit(); })
+                });
+            })(m, svg, root);
+        }
     }
+
+    var MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
+    var observer = new MutationObserver(function(mutations) {
+        update_markmaps();
+    });
+
+    var target = document.getElementById('mkdocs-decrypted-content');
+    if (undefined != target) {
+        observer.observe(target, { childList: true });
+    }
+
+    update_markmaps();
 })();
