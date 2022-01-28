ac200cb253c9ec6d9471f27d961ef4d692ace562
neatc0der <grosch.tobias@googlemail.com>
2022-01-28 22:46:41 +0100

    #36: Adds mastodon status post to distribution workflow

--- .build/mkdocs_markmap_build/distribution.py
+++ .build/mkdocs_markmap_build/distribution.py
@@ -1,16 +1,20 @@
 import os
 import sys
 from typing import List
+import requests
 
 from twine.cli import dispatch
 
-from mkdocs_markmap.__meta__ import PROJECT_NAME
-from .common import AssetDownloader, GithubHandler
+from mkdocs_markmap.__meta__ import PROJECT_NAME, REPOSITORY_URL
+from .common import AssetDownloader
+
+
+MASTODON_URL: str = "https://fosstodon.org"
 
 
 class DistributionHandler(object):
     def __init__(self, tag: str) -> None:
-        self._collector = AssetDownloader(tag)
+        self._collector: AssetDownloader = AssetDownloader(tag)
 
     def distribute(self, dry_run: bool = True):
         assets: List[str] = self._collector.get_assets_from_release()
@@ -27,3 +31,21 @@ class DistributionHandler(object):
 
         else:
             dispatch(['upload', *assets])
+
+
+class MastodonHandler(object):
+    def __init__(self, tag: str) -> None:
+        self.tag = tag
+        self._url: str = f"{REPOSITORY_URL}/releases/{tag}"
+
+    def post(self):
+        auth_token: str = os.environ.get('MASTODON_TOKEN')
+        if auth_token is None:
+            print('environment variable "MASTODON_TOKEN" is not set')
+            sys.exit(1)
+
+        response: requests.Response = requests.post(f"{MASTODON_URL}/api/v1/statuses", data={
+            "status": f"🆕 {PROJECT_NAME} {self.tag}\n{self._url}",
+        }, headers={"Authorization": f"Bearer {auth_token}"})
+        if not response.ok:
+            print(f"unable to post status on mastodon: {response.text} ({response.status_code})")
--- mkdocs_markmap/__meta__.py
+++ mkdocs_markmap/__meta__.py
@@ -1,6 +1,7 @@
-PACKAGE_NAME = 'mkdocs_markmap'
-PROJECT_NAME = PACKAGE_NAME.replace('_', '-')
-PROJECT_VERSION = '2.2.0'
+PACKAGE_NAME: str = 'mkdocs_markmap'
+PROJECT_NAME: str = PACKAGE_NAME.replace('_', '-')
+PROJECT_VERSION: str = '2.2.0'
 
-OWNER = 'neatc0der'
-REPOSITORY_NAME = f'{OWNER}/{PROJECT_NAME}'
+OWNER: str = 'neatc0der'
+REPOSITORY_NAME: str = f'{OWNER}/{PROJECT_NAME}'
+REPOSITORY_URL: str = f'https://github.com/{REPOSITORY_NAME}'
