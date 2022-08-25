10d7e5d9a3f0ff7f0fc24dcab14b9ea38813edf8
neatc0der <neatmail3r@protonmail.com>
2022-08-25 18:53:16 +0200

    Updates repository name

--- mkdocs_markmap/__meta__.py
+++ mkdocs_markmap/__meta__.py
@@ -3,5 +3,6 @@ PROJECT_NAME: str = PACKAGE_NAME.replace('_', '-')
 PROJECT_VERSION: str = '2.2.2'
 
 OWNER: str = 'neatc0der'
-REPOSITORY_NAME: str = f'{OWNER}/{PROJECT_NAME}'
+ORGANISATION: str = 'markmap'
+REPOSITORY_NAME: str = f'{ORGANISATION}/{PROJECT_NAME}'
 REPOSITORY_URL: str = f'https://github.com/{REPOSITORY_NAME}'
