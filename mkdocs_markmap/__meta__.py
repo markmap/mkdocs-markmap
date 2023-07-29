9c1fe1069d3d9edd271f09cddebb4d1681f2fdb9
Gerald <code@gera2ld.space>
2023-07-29 10:13:10 +0800

    chore: bump version to 2.4.0

--- mkdocs_markmap/__meta__.py
+++ mkdocs_markmap/__meta__.py
@@ -1,6 +1,6 @@
 PACKAGE_NAME: str = 'mkdocs_markmap'
 PROJECT_NAME: str = PACKAGE_NAME.replace('_', '-')
-PROJECT_VERSION: str = '2.3.0'
+PROJECT_VERSION: str = '2.4.0'
 
 OWNER: str = 'neatc0der'
 ORGANISATION: str = 'markmap'
